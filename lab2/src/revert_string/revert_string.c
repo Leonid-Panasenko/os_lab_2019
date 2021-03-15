26000bcbac9494f7b3f975d44f5f54d2e4398814
leonid-panasenko <leonid-panasenko@mail.ru>
2021-03-15 15:29:40 +0000

    Lab2 numbers 1-3

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,22 @@
 #include "revert_string.h"
+#include <string.h>
+#include <stdio.h>
+
+void Swap(char *left, char *right)
+{
+	char l = *left;
+    *left = *right;
+    *right = l;
+}
 
 void RevertString(char *str)
 {
-	// your code here
-}
+    int i;
+    int l = strlen(str);
+    char* newstr = "";
 
+	for(i = 0; i <= l/2; i++)
+    {                                                                 
+        Swap(str+i, str+l-i-1);
+    }
+}
